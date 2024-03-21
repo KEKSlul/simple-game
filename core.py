104e031e2d7959ec8de3f40eea2c2aa838289df6
KEKSlul <82319788+KEKSlul@users.noreply.github.com>
2024-03-21 09:33:38 +0100

    letter is part of word
    schaut ob der Buchstabe im angegebenen Wort ist

--- core.py
+++ core.py
@@ -1,7 +1,17 @@
 print("Hello and welcome to my little stupid game.")
 
-var = input("Give me a word ") 
-
+var = input("Give me a word: ") 
 
 
 print("You choose " + var)
+
+L = len(var)
+
+print("_" * L)
+
+test = input("Gimme a letter, that is part of the word: ")
+
+if test in var:
+    print("true")
+else:
+    print("false")
